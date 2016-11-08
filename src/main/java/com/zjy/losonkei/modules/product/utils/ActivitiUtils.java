82c847cfacdf50ed23566d486785f2b9783830bb
junyan-zg <275620888@qq.com>
2016-11-08 23:41:22 +0800

    [add]新产品研发流程

---
+++ src/main/java/com/zjy/losonkei/modules/product/utils/ActivitiUtils.java
@@ -0,0 +1,19 @@
+package com.zjy.losonkei.modules.product.utils;
+
+/**
+ * Created by Administrator on 2016/11/8.
+ */
+public class ActivitiUtils {
+    public final static String PROCESS_KEY_INVENT = "invent";//新产品研发流程key
+
+    public final static String VAR_MANAGER = "manager";
+
+    public final static String VAR_PRODUCTERS = "producters";
+
+    public final static String VAR_AUDITORS = "auditors";
+
+    public final static String CONDITION_CREATE_ORDERS_AGAIN = "createOrdersAgain";
+
+    public final static String CONDITION_AUDIT_OK = "auditOk";
+
+}
